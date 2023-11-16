56c6698ff040052e810f45832f6b819b53755285
Pankaj Kumar <kuldeepydv15@gmail.com>
2023-11-16 20:31:36 +0530

    contact updated

--- client/src/components/chat/menu/Menu.jsx
+++ client/src/components/chat/menu/Menu.jsx
@@ -2,7 +2,7 @@ import { Box } from '@mui/material';
 //components
 import Header from './Header';
 import Search from './Search';
-import Conversations from './conversations';
+import Conversations from './Conversations';
 
 
 
--- client/src/components/chat/menu/conversations.jsx
+++ client/src/components/chat/menu/conversations.jsx
@@ -1,37 +1,51 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useContext, Fragment } from "react";
 
-import { getUsers } from '../../../service/api';
-import { Box } from '@mui/material';
+import { Box, styled, Divider } from "@mui/material";
 
-import Conversation from './conversation';
+import { getUsers } from "../../../service/api";
+import { AccountContext } from "../../../context/AccountProvider";
+
+import Conversation from "./Conversation";
+
+
+const Component = styled(Box)`
+    height: 81vh;
+    overflow: overlay;
+
+`
+
+const StyledDivider =   styled(Divider)`
+    margin: 0 0 0 70px;
+    background: #e9edef;
+    opacity: .6;
+`
 
 const Conversations = () => {
+  const [users, setUsers] = useState([]);
 
-    const [users, setUsers] = useState([]);
+  const { account } = useContext(AccountContext);
 
-    useEffect(() => {
-        const fetchData = async ()=>{
-            let response = await getUsers();
-            // console.log(response);
-            setUsers(response);
-        }
-        fetchData();
+  useEffect(() => {
+    const fetchData = async () => {
+      let response = await getUsers();
+      console.log(response);
+      setUsers(response);
+    };
+    fetchData();
+  }, []);
 
-    }, []);
-
-    return(
-        <Box>
-            {
-                users.map((user)=>{
-                    console.log("user:",user);
-                    return (
-                        <>
-                    <Conversation/>{user.name}</>
-                    )
-                })
-            }
-        </Box>
-    )
-}
+  return (
+    <Component>
+      {users.map((user, index) => {
+        return (
+          <Fragment key={index}>
+            {user.sub !== account.sub && <Conversation user={user} /> }
+            <StyledDivider/>
+          </Fragment>
+        );
+      })}
+    </Component>
+  );
+};
 
 export default Conversations;
